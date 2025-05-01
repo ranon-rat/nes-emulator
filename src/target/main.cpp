acd1450e97e738ebc42d4157c7a334adab448172
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2025-05-01 09:40:44 -0600

    raylib window

--- src/target/main.cpp
+++ src/target/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <raylib/raylib.h>
 
-int main(){
-    std::cout<<"hello world\n";
-    return 0;
+int main() {
+  const int screenWidth = 800;
+  const int screenHeight = 600;
+  InitWindow(screenWidth, screenHeight, "raylib basic window");
+  SetTargetFPS(60);
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    for(int i=0;i<10;i++){
+        DrawText("It works!", 20+i*20, 20*i, 20, BLACK);
+    }
+    EndDrawing();
+  }
+  CloseWindow();
+  return 0;
 }
